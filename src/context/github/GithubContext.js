cde7167cc80edf10ed41827091a2cc66b30c8ae6
AkshitaBajaj2112 <AkshitaBajajCa@gmail.com>
2024-01-22 00:44:20 -0500

    Search user working right

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,59 +1,56 @@
-import { createContext , useState} from 'react'
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
+import { useSearchParams } from 'react-router-dom';
 
-
-const GithubContext = createContext()
+const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: false,
+  };
 
-export const GithubProvider = ({children}) =>{
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  // search users
+  const searchUsers = async (text) => {
+    setLoading();
 
-    const fetchUsers = async () => {
-        try {
-            const response = await fetch(`${GITHUB_URL}/users`, {
-                method: 'GET',
-                headers: {
-                  Authorization: `token ${GITHUB_TOKEN}`,
-                  'User-Agent': 'github_finder_udemy',
-                  // Remove the Cache-Control header
-                },
-              
-          });
-    
-          if (!response.ok) {
-            const errorMessage = await response.text();
-            throw new Error(`GitHub API error: ${response.status} - ${errorMessage}`);
-          }
-    
-          const contentType = response.headers.get("content-type");
-          if (!contentType || !contentType.includes("application/json")) {
-            console.error("Unexpected response format. Expected JSON but received:", await response.text());
-            // Handle the unexpected response format accordingly
-            return;
-          }
-    
-          const data = await response.json();
-          setUsers(data);
-          setLoading(false);
-        } catch (error) {
-          console.error("Error fetching users:", error.message);
-        }
-      };
-    
+    const params = new URLSearchParams({
+      q: text,
+    });
 
-      return <GithubContext.Provider value={{
-        users, 
-        loading,
-        fetchUsers
-      }}>
-        {children}
-      </GithubContext.Provider>
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+        'User-Agent': 'github_finder_udemy',
+        // Remove the Cache-Control header
+      },
+    });
 
+    const { items } = await response.json();
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    });
+  };
 
-}
+  // set loading
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
-export default GithubContext
+  return (
+    <GithubContext.Provider value={{
+      users: state.users,
+      loading: state.loading,
+      searchUsers,
+    }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
